6dce6b8408aff73b84b597e02e9a5f95ec2adc96
Gabriel Enslein <gabrielenslein@Gabriels-MacBook-Pro-2.local>
2016-08-07 19:14:46 -0400

    ephemeral ports working

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,6 +20,10 @@ template '/etc/nginx/sites-available/default.conf' do
   mode '0755'
 end
 
+execute 'change ephemeral ports' do
+  command 'sudo sysctl -w net.ipv4.ip_local_port_range="20000 64000"'
+end
+
 link '/etc/nginx/sites-enabled/default' do
   to '/etc/nginx/sites-available/default.conf'
   notifies :restart, 'service[nginx]'
