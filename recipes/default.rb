39a93a6d39264e2cfc765bac514fa6d09ddeaf3a
Gabe Enslein <gabe.enslein@careerbuilder.com>
2016-08-08 12:22:42 -0400

    push for test

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,11 +20,11 @@ template '/etc/nginx/sites-available/default.conf' do
   mode '0755'
 end
 
-execute 'change ephemeral ports' do
-  command 'sudo sysctl -w net.ipv4.ip_local_port_range="20000 64000"'
-end
-
 link '/etc/nginx/sites-enabled/default' do
   to '/etc/nginx/sites-available/default.conf'
   notifies :restart, 'service[nginx]'
+end
+
+execute 'change ephemeral ports' do
+  command 'sudo sysctl -w net.ipv4.ip_local_port_range="20000 64000"'
 end
